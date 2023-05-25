4e64e47e2494fe93252b00c346d37e820cf5fd82
jacob <jacobfl2013@gmail.com>
2023-05-25 19:30:41 +1200

    Added submenus for navbar and changed detection for content grid

--- app/appleNavbar.tsx
+++ app/appleNavbar.tsx
@@ -1,37 +1,117 @@
-"use client"
+"use client";
 import "react";
 import { use, useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import ExpandNavbar from "./appleNabarExpand";
 
+export default function Navbar() {
+  let [hover, setHover] = useState(false);
+  let [item, setItem] = useState("");
 
-export default function Navbar() { 
-    let [hover, setHover] = useState(false);
-    let [item, setItem] = useState("");
-    
-    let ref = useRef<any>();
-    return ( 
-        <div className="z-4">
-            <div className='bg-gray-500 h-[50px] relative'>
-                    <ul onMouseOverCapture={() => {setItem("");}} className='flex pt-[12px] w-auto justify-center text-neutral-100 text-sm'>
-                        <img id="start" className='flex w-[19.6px] h-[24px]' src="/Apple_logo_black.png"/>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Store")}}  className='px-5'>Store</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Mac")}}  className='px-5'>Mac</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("iPad")}}  className='px-5'>iPad</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("iPhone")}}  className='px-5'>iPhone</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Watch")}}  className='px-5'>Watch</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Airpods")}}  className='px-5'>Airpods</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("TV")}}  className='px-5'>Tv & Home</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Entertainment")}}  className='px-5'>Entertainment</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Accessories")}}  className='px-5'>Accessories</p>
-                        <p onMouseOverCapture={() => {setHover(true);setItem("Support")}}  className='px-5'>Support</p>
-                    </ul>
-                    {hover && 
-                        <ExpandNavbar hover={hover} setHover={setHover} prop={item}/>
-                    }
-
-                </div>
-        </div>
-
-    )
+  let ref = useRef<any>();
+  return (
+    <div className="bg-[#434344]">
+      <div className="h-[50px] relative">
+        <ul
+          onMouseOverCapture={() => {
+            setItem("");
+          }}
+          className="flex pt-[12px] w-auto justify-center text-neutral-100 text-sm">
+          <img
+            id="start"
+            className="flex w-[19.6px] h-[24px]"
+            src="/Apple_logo_black.png"
+          />
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Store");
+            }}
+            className="px-5">
+            Store
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Mac");
+            }}
+            className="px-5">
+            Mac
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("iPad");
+            }}
+            className="px-5">
+            iPad
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("iPhone");
+            }}
+            className="px-5">
+            iPhone
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Watch");
+            }}
+            className="px-5">
+            Watch
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Airpods");
+            }}
+            className="px-5">
+            Airpods
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("TV");
+            }}
+            className="px-5">
+            Tv & Home
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Entertainment");
+            }}
+            className="px-5">
+            Entertainment
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Accessories");
+            }}
+            className="px-5">
+            Accessories
+          </p>
+          <p
+            onMouseOverCapture={() => {
+              setHover(true);
+              setItem("Support");
+            }}
+            className="px-5">
+            Support
+          </p>
+        </ul>
+        {hover && (
+          <ExpandNavbar
+            hover={hover}
+            setHover={setHover}
+            prop={item}
+            setItem={setItem}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
