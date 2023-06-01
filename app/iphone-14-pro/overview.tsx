9a8ede1f82438ef047fcddf68ed54977de25fa96
jacob <jacobfl2013@gmail.com>
2023-06-01 14:49:15 +1200

    Added fixed position toggle for overiew + theme selector

--- app/iphone-14-pro/overview.tsx
+++ app/iphone-14-pro/overview.tsx
@@ -1,8 +1,24 @@
 "use client";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useState } from "react";
+gsap.registerPlugin(ScrollTrigger)
+
 export default function Overview() {
+    let [fixed, setFixed] = useState(false);
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            console.log(window.scrollY);
+            if (window.scrollY >= 50) {
+                setFixed(true);
+            } else {
+                setFixed(false);
+            }
+        })
+    })
     return (
-        <div className="w-auto h-auto flex pt-4 text-white bg-black">
+        <div id='overview' className={"w-full h-auto flex py-2 text-white bg-black " + (fixed ? " fixed top-0 z-50" : null)}>
             <p className="ml-[201px] text-2xl font-semibold">iPhone 14 Pro</p>
             <ul className="flex w-auto mx-auto text-sm py-2 pl-40">
                 <p className="mx-2 text-gray-400">Overview</p>
--- app/iphone-14-pro/photoGallery.tsx
+++ app/iphone-14-pro/photoGallery.tsx
@@ -11,6 +11,18 @@ export default function PhotoGallery() {
     let [current, setCurrent] = useState({ color: colours[0] });
     let [ended, setEnded] = useState(false);
     let [src, setSrc] = useState(current.color.src)
+    let [fixed, setFixed] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            console.log(window.scrollY);
+            if (window.scrollY >= 75) {
+                setFixed(true);
+            } else {
+                setFixed(false);
+            }
+        })
+    })
 
     function changeState(color: string) {
         colours.forEach((item: Colour, index: number) => {
@@ -42,7 +54,7 @@ export default function PhotoGallery() {
                 <div id="photogallery">
                     <div className="flex">
                         <p className="text-gray-600 font-semibold w-auto h-auto text-6xl ml-[120px] opacity-90 mt-6">Pro. Beyond</p>
-                        <ul className="flex w-auto justify-center align-center mt-[70px] ml-[400px]">
+                        <ul className={(fixed ? "fixed flex z-50 justify-center w-full pl-[740px] backdrop-blur-xl py-[10px]" : "flex w-auto justify-center align-center mt-[70px] ml-[400px]")}>
                             <p id="col" className="text-white font-semibold text-xl mr-6 text-right w-[150px] fadein">{current.color.text}</p>
                             <div onClick={() => { changeState("Deep Purple") }} className={"rounded-full mr-4 relative w-[30px] h-[30px] bg-black " + (current.color.text == 'Deep Purple' ? " border-blue-500 border-2" : null)}>
                                 <button className="h-[20px] w-[20px] rounded-full bg-purple-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></button>
@@ -62,6 +74,6 @@ export default function PhotoGallery() {
                     <img id='img' src={current.color.src} className="pt-[65px] mx-auto fadein" />
                 </div>
             }
-        </div>
+        </div >
     )
 }
